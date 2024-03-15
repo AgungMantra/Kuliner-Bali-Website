c74df86525e246dd1842c3f7ce62f63aee6538f3
AgungMantra <agung.mantra07@gmail.com>
2024-03-15 22:41:39 +0800

    layout done

---
+++ script.js
@@ -0,0 +1,45 @@
+// ========== UNTUK TOMBOL NAV ==========
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav');
+const navbar = document.querySelector('.navbar');
+const navlayout = document.querySelector('.nav-layout');
+const control = document.querySelector('.control');
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    nav.classList.toggle('active');
+    navbar.classList.toggle('active'); 
+    navlayout.classList.toggle('active'); 
+    control.classList.add('active');
+});
+
+control.addEventListener("click", () => {
+    navlayout.classList.toggle('active-control');
+    navlayout.classList.remove('active'); 
+    control.classList.remove('active');
+    navbar.classList.remove('active'); 
+    hamburger.classList.remove("active");
+    nav.classList.remove('active');
+});
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    nav.classList.remove("active");
+    navlayout.classList.remove("active-control");
+    control.classList.remove('active');
+}));
+
+// ========== Scroll Reveal ==========
+ScrollReveal({
+    reset: false,
+    distance: '30px',
+    duration: 800,
+    delay: 100
+});
+
+ScrollReveal().reveal('.texthome, .text, .wisata, .tiket, .layout-box, .perusahaan, .bayarimg, .kontak-container', { origin: 'bottom' });
+
+
+// test
+
+
